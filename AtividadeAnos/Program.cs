74125cdb6b2e91c64bf42da1b59cb366884c8ef5
Usuario <Usuario@CPD02-M07>
2024-02-27 11:53:59 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadeAnos/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeAnos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Informe sua idade em anos: ");
+            int anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe quantos meses se passaram desde seu aniversario?: ");
+            int meses = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informe quantos dias se passaram desde seu aniversário?: ");
+            int dias = int.Parse(Console.ReadLine());
+
+            anos = anos * 365;
+            meses = meses * 30;
+            dias = anos + meses + dias;
+
+            Console.WriteLine("Sua idade em dias é: " + dias);
+            Console.ReadKey();
+
+        
+        }
+    }
+}
